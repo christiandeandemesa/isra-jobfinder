00b3ba75c244fed23db83cd3a26fa8ee2d86b580
Christian Dean <christiandeandemesa@gmail.com>
2023-05-11 16:21:49 -0700

    NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+import Navbar from "./components/NavBar/NavBar";
+import Search from "./components/SearchDiv/Search";
+import Jobs from "./components/JobDiv/Jobs";
+import Value from "./components/ValueDiv/Value";
+import Footer from "./components/FooterDiv/Footer";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <div className="w-[85%] m-auto bg-white">
+      <Navbar />
+      <Search />
+      <Jobs />
+      <Value />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
+
